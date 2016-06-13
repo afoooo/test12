f35adbce1afbb2b0a52cdb28bf01650a4e57dbc2
Alex Fu <jslolala@gmail.com>
2016-06-13 17:06:36 -0400

    first draft

---
+++ Desktop/NYCDA/Week 2/Day 1/Homework/cheat sheet/main.js
@@ -0,0 +1,19 @@
+//how to set variables
+var car={make: 'toyota', mode: 'prius'};
+console.log(car['make']);
+
+//check if two strings are equal, including object type
+console.log("stringone" === "string two");
+
+//To test if two strings are NOT equal://
+console.log("stringone" == "string two");
+
+//if statement//
+
+if(5>10){
+console.log("A");
+}
+if(5<10){
+console.log("B");
+}
+
